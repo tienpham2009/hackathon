cb4df183bf48ade40378c8b7417f4c49c3254faf
TamNguyen <ngductam91@gmail.com>
2021-06-19 18:23:37 +0700

    update

--- resources/views/users/detail.blade.php
+++ resources/views/users/detail.blade.php
@@ -1,7 +1,5 @@
 @extends('home')
 
-@section('title', 'Cập nhật công viêc')
-
 @section('content')
 
     <div class="row">
@@ -10,28 +8,28 @@
         </div>
 
         <div class="col-md-12">
-                <table class="table table-striped">
-                    <tr>
-                        <td>Full name</td>
-                        <td>Image</td>
-                        <td>Ingame</td>
-                        <td>Birth date</td>
-                        <td>Email</td>
-                        <td>Gender</td>
-                    </tr>
-                    <tr>
-                        <td>{{$user->name}}</td>
-                        <td>
-                            <img src="" alt="">
-                        </td>
-                        <td>{{$user->name_ingame}}</td>
-                        <td>{{$user->birth_date}}</td>
-                        <td>{{$user->email}}</td>
-                        <td>{{$user->gender}}</td>
-                    </tr>
-                </table>
+            <table class="table table-striped">
+                <tr>
+                    <td>Full name</td>
+                    <td>Image</td>
+                    <td>Ingame</td>
+                    <td>Birth date</td>
+                    <td>Email</td>
+                    <td>Gender</td>
+                </tr>
+                <tr>
+                    <td>{{$user->name}}</td>
+                    <td>
+                        <img src="" alt="">
+                    </td>
+                    <td>{{$user->name_ingame}}</td>
+                    <td>{{$user->birth_date}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->gender}}</td>
+                </tr>
+            </table>
 
-                <a href="{{route('users.list')}}" class="btn btn-secondary">Back</a>
+            <a href="{{route('users.list')}}" class="btn btn-secondary">Back</a>
         </div>
     </div>
 @endsection
